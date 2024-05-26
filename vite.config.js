ea3292db516ab7c6a7fc3e9ff114baa7284a5916
Szymon <szymon.koryczan.kontakt@gmail.com>
2024-05-26 19:07:47 +0200

    Próba naprawy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  plugins: [react(), mkcert(), VitePWA({
     base: "/React/",
     registerType: 'prompt',
     injectRegister: false,
