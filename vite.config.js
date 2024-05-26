1478e5777d59582e0815173413e3bc6bad967382
Szymon <szymon.koryczan.kontakt@gmail.com>
2024-05-26 20:13:46 +0200

    usuniecie badge pwa

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,57 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert(), VitePWA({
-    base: "/React/",
-    registerType: 'prompt',
-    injectRegister: false,
-
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-
-    manifest: {
-      name: 'ReactPWA',
-      short_name: 'ReactPWA',
-      description: 'Test aplikacji PWA z powiadomieniami ',
-      theme_color: '#ffffff',
-    },
-
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
-
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+  base: '/React/', // Ustaw tutaj ścieżkę bazową repozytorium
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'ReactPWA',
+        short_name: 'ReactPWA',
+        description: 'Test aplikacji PWA z powiadomieniami',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/React/',
+        icons: [
+          {
+            src: 'icons/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshots/640x480.png',
+            sizes: '640x480',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+      },
+    }),
+  ],
+});
